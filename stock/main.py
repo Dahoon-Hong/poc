9f21d7c472d328d6f6b1ee51cc38eee5e2f48479
pos9906 <slayerboxr@gmail.com>
2020-06-08 23:38:40 +0900

    add transaction logic. need to split

--- stock/main.py
+++ stock/main.py
@@ -1,5 +1,5 @@
-from datetime import date
-
+from datetime import datetime, date
+import pandas as pd
 from stock import StockInformation
 import config
 
@@ -8,75 +8,67 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
 
+def buy_stock(df, t_date, amount, total_price):
+    condition = df['date'] >= t_date
+    df.loc[condition, 'buy_stack'] += total_price
+    df.loc[condition, 'amount_stack'] += amount
+
+
+
+
 if __name__ == "__main__":
     stock = StockInformation()
     code_list = stock.get_code_list()
 
-    for name in config.noi:
-        try:
-            code = code_list[code_list['name'] == name]['code'].item()
-        except ValueError:
-            print("CANNOT FOUND CODE FOR NAME [%s]" % name)
-            continue
+#    for name in config.noi:
+    name = '현대자동차'
+    try:
+        code = code_list[code_list['name'] == name]['code'].item()
+    except ValueError:
+        print("CANNOT FOUND CODE FOR NAME [%s]" % name)
+        #continue
 
-        print(code)
-        df = stock.get_stock_information(code, date(2020, 6, 1))
-        print(df)
+    print(code)
+    df = stock.get_stock_information(code, date(2019, 8, 1))
+    #df = stock.get_stock_information(code, date(2020, 6, 1))
+    print(df)
 
-        # transaction init
-        dfs = df.copy()
-        dfs['buy_stack'] = 0
-        dfs['amount_stack'] = 0
-        dfs['profit'] = 0
+    # transaction init
+    dfs = df.copy()
+    dfs['buy_stack'] = 0
+    dfs['amount_stack'] = 0
+    dfs['profit'] = 0
+    '''
+    t_df = pd.read_csv('./res/input/%s.csv'%code, encoding='CP949')
+    print(t_df)
+    t_df.columns = ['name', 'date', 'category', 'amount' ,'amount_krw']
+    t_df.dropna(inplace=True)
+    t_df['date'] = t_df['date'].apply(lambda x : datetime.strptime(str(x)[:-2], "%y%m%d").date())
+    t_df['amount_krw'] = t_df['amount_krw'].apply(lambda x : x if x > 0 else x*-1)
+    t_df['category'] = t_df['category'].apply(lambda x : 'buy' if x == '매수' else 'sell')
+    print(t_df)
 
-        # buy 2020-05-27, 5930, 50000, 10
-        condition = dfs['date'] >= date(2020, 5, 27)
-        dfs.loc[condition, 'buy_stack'] += 50000 * 10
-        dfs.loc[condition, 'amount_stack'] += 10
-        print(dfs)
+    # buy 2020-05-27, 5930, 50000, 10
+    t_df[t_df['category'] == 'buy'].apply(lambda x : buy_stock(dfs, x['date'], x['amount'], x['amount_krw']), axis=1)
+    '''
 
-        # buy 2020-05-27, 5930, 49000, 5
-        condition = dfs['date'] >= date(2020, 5, 27)
-        dfs.loc[condition, 'buy_stack'] += 49000 * 5
-        dfs.loc[condition, 'amount_stack'] += 5
-        print(dfs)
 
-        # buy 2020-05-28, 5930, 50700, 30
-        condition = dfs['date'] >= date(2020, 5, 31)
-        dfs.loc[condition, 'buy_stack'] += 50700 * 30
-        dfs.loc[condition, 'amount_stack'] += 30
-        print(dfs)
-        
-        dfs['profit'] = dfs['closing'] * dfs['amount_stack'] - dfs['buy_stack']
-        print(dfs)
+    dfs['profit'] = dfs['closing'] * dfs['amount_stack'] - dfs['buy_stack']
+    print(dfs)
 
-        '''
-        fig.update_xaxes(
-            rangeslider_visible=True,
-            rangeselector=dict(
-                buttons=list([
-                    dict(count=1, label="1m", step="month", stepmode="backward"),
-                    dict(count=6, label="6m", step="month", stepmode="backward"),
-                    dict(count=1, label="YTD", step="year", stepmode="todate"),
-                    dict(count=1, label="1y", step="year", stepmode="backward"),
-                    dict(step="all")
-                ])
-            )
-        )
-        '''
-        fig = make_subplots(specs=[[{"secondary_y": True}]])
-        fig.add_trace(
-            go.Scatter(x=dfs['date'], y=dfs['profit'], mode='lines', name='profit'),
-            secondary_y=False
-        )
-        fig.add_trace(
-            go.Scatter(x=dfs['date'], y=dfs['closing'], mode='lines', name='closing'),
-            secondary_y=True
-        )
-        fig.update_layout(
-            title_text="[%s] profit" % code,
-            yaxis=dict(tickformat=",000"),
-            yaxis2=dict(tickformat=",000"),
-        )
-        fig.update_xaxes(title_text="date")
-        fig.show()
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+    fig.add_trace(
+        go.Scatter(x=dfs['date'], y=dfs['profit'], mode='lines', name='profit'),
+        secondary_y=False
+    )
+    fig.add_trace(
+        go.Scatter(x=dfs['date'], y=dfs['closing'], mode='lines', name='closing'),
+        secondary_y=True
+    )
+    fig.update_layout(
+        title_text="[%s] profit" % code,
+        yaxis=dict(tickformat=",000"),
+        yaxis2=dict(tickformat=",000"),
+    )
+    fig.update_xaxes(title_text="date")
+    fig.show()
